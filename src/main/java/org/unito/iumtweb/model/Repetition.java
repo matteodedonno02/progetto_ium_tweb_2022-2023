868625cd27839fc2360b10456f78ec68d2a70747
Matteo De Donno <matteodedonno02@gmail.com>
2022-11-19 14:26:20 +0100

    Some method for repetition

---
+++ src/main/java/org/unito/iumtweb/model/Repetition.java
@@ -0,0 +1,82 @@
+package org.unito.iumtweb.model;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class Repetition {
+    private int idRepetition;
+    private User user;
+    private Teaching teaching;
+    private int state;
+    private Date date;
+    private Time time;
+
+    public Repetition(int idRepetition, User user, Teaching teaching, int state, Date date, Time time) {
+        this.idRepetition = idRepetition;
+        this.user = user;
+        this.teaching = teaching;
+        this.state = state;
+        this.date = date;
+        this.time = time;
+    }
+
+    public int getIdRepetition() {
+        return idRepetition;
+    }
+
+    public void setIdRepetition(int idRepetition) {
+        this.idRepetition = idRepetition;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Teaching getTeaching() {
+        return teaching;
+    }
+
+    public void setTeaching(Teaching teaching) {
+        this.teaching = teaching;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return "Repetition{" +
+                "idRepetition=" + idRepetition +
+                ", user=" + user +
+                ", teaching=" + teaching +
+                ", state=" + state +
+                ", date=" + date +
+                ", time=" + time +
+                '}';
+    }
+}
