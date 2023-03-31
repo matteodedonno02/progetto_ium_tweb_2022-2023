78b7fadc51c22f6c623283dda72e1fd9c7456a03
Matteo De Donno <matteodedonno02@gmail.com>
2023-03-31 18:53:50 +0200

    refactoring login page. Addedd concurrently

--- src/main/webapp/progetto_ium_tweb_frontend/src/view/LoginPage.vue
+++ src/main/webapp/progetto_ium_tweb_frontend/src/view/LoginPage.vue
@@ -1,22 +1,26 @@
 <template>
     <div class="container login-container d-flex justify-content-center align-items-center">
         <div class="w-25">
-            <div class="">
-                <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-                <div class="col-sm-10">
-                    <input v-model="email" type="text" class="form-control" id="staticEmail"
-                        placeholder="Inserisci la tua email">
+            <h3>Login</h3>
+            <p>Effettua l'accesso per continuare</p>
+            <div class="group position-relative">
+                <label for="inputEmail" class="custom-placeholder col-sm-2 col-form-label position-absolute">Email</label>
+                <div class="">
+                    <input v-on:change="animate" v-on:onfocus="animations" v-model="email" type="text"
+                        class="form-control shadow-none" id="inputEmail">
                 </div>
             </div>
-            <div class="">
-                <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-                <div class="col-sm-10">
-                    <input v-model="password" type="password" class="form-control" id="inputPassword"
-                        placeholder="Inserisci la tua password...">
+            <div class="group position-relative pt-3">
+                <label for="inputPassword"
+                    class="custom-placeholder col-sm-2 col-form-label position-absolute">Password</label>
+                <div class="">
+                    <input v-model="password" type="password" class="form-control shadow-none" id="inputPassword">
                 </div>
             </div>
-            <div class="pt-3">
-                <button v-on:click="login" type="button" class="btn btn-primary">Entra</button>
+            <div class="pt-3 d-flex justify-content-center">
+                <button v-on:click="login" type="button" class="custom-button btn btn-primary rounded-pill">
+                    <mdiArrowRightCircleOutline /> Login
+                </button>
             </div>
         </div>
     </div>
@@ -24,9 +28,13 @@
 
 <script>
 import $ from 'jquery'
+import mdiArrowRightCircleOutline from "vue-material-design-icons/ArrowRightCircleOutline.vue"
 
 export default {
     name: "LoginPage",
+    components: {
+        mdiArrowRightCircleOutline
+    },
     props: ["loggedUser"],
     data() {
         return {
@@ -52,6 +60,22 @@ export default {
                     self.$emit("set-logged-user", data)
                 }
             })
+        },
+    },
+    watch: {
+        email(newEmail) {
+            if (newEmail === "") {
+                $("label[for='inputEmail']").removeClass("focus-label")
+            } else {
+                $("label[for='inputEmail']").addClass("focus-label")
+            }
+        },
+        password(newPassword) {
+            if (newPassword === "") {
+                $("label[for='inputPassword']").removeClass("focus-label")
+            } else {
+                $("label[for='inputPassword']").addClass("focus-label")
+            }
         }
     }
 }
